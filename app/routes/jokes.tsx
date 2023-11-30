172664d0cd5b13f9c870c65ea8b32cea28eec97b
James Kent <jameskent91@gmail.com>
2023-11-30 13:35:36 +0000

    Adding styles and routes

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,5 +1,11 @@
 import { Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
 
+import stylesUrl from "~/styles/jokes.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
 export default function JokesRoute() {
   return (
     <div>
