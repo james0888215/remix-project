a9e78dc34527ee2c8603a66ab5d28a94045efa27
James Kent <jameskent91@gmail.com>
2023-12-08 21:52:27 +0000

    Add sessions and cookies

--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -4,6 +4,8 @@ import { useActionData } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
 import { badRequest } from "~/utils/request.server";
+import { requireUserId } from "~/utils/session.server";
+
 
 function validateJokeContent(content: string) {
   if (content.length < 10) {
@@ -20,6 +22,7 @@ function validateJokeName(name: string) {
 export const action = async ({
   request,
 }: ActionFunctionArgs) => {
+  const userId = await requireUserId(request);
   const form = await request.formData();
   const content = form.get("content");
   const name = form.get("name");
@@ -47,7 +50,9 @@ export const action = async ({
     });
   }
 
-  const joke = await db.joke.create({ data: fields });
+  const joke = await db.joke.create({
+    data: { ...fields, jokesterId: userId },
+  });
   return redirect(`/jokes/${joke.id}`);
 };
 
